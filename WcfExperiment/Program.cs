edef1a57cd581b10755b02031ff6d1b3ede2ab9e
gurza <gurza000@gmail.com>
2018-07-01 00:29:41 +0300

    Initial check-in of module WcfExperiment

---
+++ WcfExperiment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WcfExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
